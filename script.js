98c7e03b9d4456e77340fb8d12e19e4a65457771
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-03-26 18:26:24 +0530

    Iterables and Generators - print values in the consoles

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* import { API_KEY as key } from './variables.js'; //Import and Rename
 console.log(key); */
 let spinner = document.querySelector(".spinner"); //Spinner to show when fetching api data
-spinner.style.display = 'none';
 const createNode = (element) => document.createElement(element); //Creation of an element
 const append = (parent, el) => parent.appendChild(el); //Function to append child to parent
 const ul = document.getElementById('newsArticlesList'); // Unordered list refrence
@@ -13,11 +12,10 @@ const channelUILists = document.querySelectorAll('.thumbnail');
 const authorNames = () => {
     let validator = {
         get(target, key) {
-            return key in target ? taget[key] : "No Author"
+            return key in target ? taget[key] : "No Author";
         }
     }
     let authors = new Proxy({}, validator);
-    console.log(authors);
 }
 
 const api = (source) => fetch(`${BASE_URL}`+source+`&apiKey=${API_KEY}`); //Fetch api function
@@ -67,5 +65,38 @@ const cnnNews = new NewsChannel('cnn');
 const associatedNews = new NewsChannel('associated-press');
 
 
+//Iterators Example - When Done is True - Stopping the Sum
+let arrayItems = new Set([1, 2, 3, 4]);
+let iterateExample = arrayItems[Symbol.iterator]();
+const iteratorFn = () => {    
+    for(let item of arrayItems) {        
+        if(iterateExample.next().done == false ) { //Condition to check if Done id false 
+            setTimeout(()=> {                
+                item++;
+                console.log("Value of an Iterable array", item);                
+            }, 1000);
+        }    
+    }    
+}
+iteratorFn();
 
+//Generator Example 1
+function* generator() {
+    yield* [1,2,3,4];
+}
+let generatorExample = generator();
+console.log(generatorExample.next());
 
+//Generator Example 2
+var iteration = {
+    *[Symbol.iterator]() {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+    }
+}
+
+for (let value of iteration) { 
+    console.log(value); 
+}
