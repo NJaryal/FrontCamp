e1589a79854f6257916c22b92cdfe912ea3dfaf5
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-04-10 18:13:00 +0530

    Separate configuration for dev and prod using common webpack file along with webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -35,9 +36,9 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
         template: './client/src/index.html'
-        //chunks: ['file1','file2']
-    })
-]
+        })
+    ]
 }
