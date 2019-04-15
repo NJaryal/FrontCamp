6161c507de051bbd1dc99b93b70140363596ea0b
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-04-15 21:03:03 +0530

    custom-loader small change - In progress

--- loaders/custom-loader.js
+++ loaders/custom-loader.js
@@ -1,5 +1,11 @@
 const oldJSONData = require('../Data.json')
-
 module.exports = function(source) {
+    for (const key in oldJSONData.employees) {
+        if (oldJSONData.hasOwnProperty(key) == isNaN) {
+          console.log("Strings Data");   
+        }else{
+          console.log("Number");
+        }
+      }
     return source;
 }
